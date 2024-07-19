251e6c017f2c6cfbe7e82c78b7977276fc1b3d95
Cocodrulo <cocodrulojr@gmail.com>
2024-07-19 15:02:23 +0100

    feat: Improve notification position customization

--- html/script.js
+++ html/script.js
@@ -4,30 +4,47 @@ const $$ = (query) => document.querySelectorAll(query);
 let Queue = [];
 
 const positions = {
-	1: { x: "0", y: "0", anim: "from-left" },
+	1: { x: "0", y: "0", anim: "from-left from-top" },
 	2: { x: "37.5%", y: "0", anim: "from-top" },
-	3: { x: "100%", y: "0", anim: "from-right" },
+	3: { x: "75%", y: "0", anim: "from-right from-top" },
 	4: { x: "0", y: "37.5%", anim: "from-left" },
 	5: { x: "75%", y: "37.5%", anim: "from-right" },
-	6: { x: "0", y: "75%", anim: "from-left" },
+	6: { x: "0", y: "75%", anim: "from-left from-bottom" },
 	7: { x: "37.5%", y: "75%", anim: "from-bottom" },
-	8: { x: "75%", y: "75%", anim: "from-right" },
+	8: { x: "75%", y: "75%", anim: "from-right from-bottom" },
 };
 
 class Notification {
-	constructor(text, type, time) {
+	constructor(text, type = "info", time = 3000) {
 		this.msg = text;
-		this.type = type || "info";
-		this.time = time || 3000;
+		this.type = type;
+		this.time = time;
 		this.notification = document.createElement("div");
 		this.inScreen = false;
 	}
 
 	static SetUpPosition(pos) {
+		if (typeof pos !== "number" || Number.isNaN(pos)) return;
+		if (pos < 1 || pos > 8) return;
+
+		$$(".position").forEach((pos) => {
+			pos.classList.remove("active");
+		});
+		$(`.position[data-position="${pos}"]`).classList.add("active");
+
+		window.localStorage.setItem("notificationPosition", pos);
+
+		$(".notifications").classList.remove("from-left");
+		$(".notifications").classList.remove("from-right");
+		$(".notifications").classList.remove("from-top");
+		$(".notifications").classList.remove("from-bottom");
+
 		const POSITION = positions[pos];
 		$(".notifications").style.left = POSITION.x;
 		$(".notifications").style.top = POSITION.y;
-		$(".notifications").classList.add(POSITION.anim);
+		POSITION.anim.split(" ").forEach((anim) => {
+			$(".notifications").classList.add(anim);
+		});
 	}
 
 	static GetApropiateIcon(type) {
@@ -87,10 +104,28 @@ function createNotification(text, type, time) {
 	}
 }
 
-function testNotify() {
-	createNotification("This is an info notification", "info");
+function testNotify(type = "info") {
+	createNotification("This is an info notification", type, 2000);
+}
+
+function openCustomization() {
+	$(".menu").classList.toggle("show");
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-	Notification.SetUpPosition(7);
+	Notification.SetUpPosition(
+		Number(window.localStorage.getItem("notificationPosition")) || 1
+	);
+
+	$$(".position").forEach((pos) => {
+		pos.addEventListener("click", () => {
+			Notification.SetUpPosition(Number(pos.dataset["position"]), pos);
+		});
+	});
+
+	$$(".buttons button").forEach((btn) => {
+		btn.addEventListener("click", (e) => {
+			createNotification("Esto es una prueba", e.target.id);
+		});
+	});
 });
