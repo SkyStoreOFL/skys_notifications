01b4daa2084b91cedca752e21cbb3f617b8f5763
Cocodrulo <cocodrulojr@gmail.com>
2024-08-04 18:02:34 +0100

    refactor: Improve user customization and notification styles

--- client/main.lua
+++ client/main.lua
@@ -1,10 +1,23 @@
 local sendConfig = true
 
+CheckFirstTime = function ()
+    if not sendConfig then return end
+    SendNUIMessage({
+        action = "config",
+        defaultPosition = Config.defaultPosition,
+        defaultDuration = Config.defaultDuration,
+        lang = Config.Lang,
+    })
+    sendConfig = false
+end
+
 --- Send a notification to the NUI
 --- @param message string
 --- @param type string
 --- @param duration number
 NewNotification = function (message, type, duration, icon, title)
+    CheckFirstTime()
+
     SendNUIMessage({
         action = "notification",
         text = message,
@@ -20,15 +33,7 @@ exports('NewNotification', NewNotification)
 if Config.userCustomize then
     --- Open the customization menu
     OpenCustomizationMenu = function ()
-        if sendConfig then
-            SendNUIMessage({
-                action = "config",
-                defaultPosition = Config.defaultPosition,
-                defaultDuration = Config.defaultDuration,
-                lang = Config.Lang,
-            })
-            sendConfig = false
-        end
+        CheckFirstTime()
     
         SetNuiFocus(true, true)
         SendNUIMessage({ action = "openCustomization" })
