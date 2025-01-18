10d91e46cfa72f6b906fded0445140316629aac4
Cocodrulo <142546774+Cocodrulo@users.noreply.github.com>
2025-01-18 19:27:20 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version "cerulean"
 game "gta5"
 
 author "Cocodrulo"
-version "1.0.0"
+version "1.1.0"
 description "Notification script compatible with any Framework"
 
 ui_page "html/index.html"
